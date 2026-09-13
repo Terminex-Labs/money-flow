0baab0945aaad8eebcafd03e5d999960698edb05
texno stalin <texno.stalin@gmail.com>
2026-09-13 16:29:38 +0300

    feat: Создана страница account-manage

---
+++ web/money-flow-ui/src/features/account/account.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from "@angular/router";
+
+export const ACCOUNT_ROUTES: Routes = [
+    { path: 'account', loadComponent: () => import('./pages/management/account-manage.component').then(component => component.AccountManageComponent) }
+];
---
+++ web/money-flow-ui/src/features/account/pages/management/account-manage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-account-manage',
+    templateUrl: 'account-manage.component.html',
+    styleUrls: ['account-manage.component.scss'],
+    standalone: true
+})
+
+export class AccountManageComponent {
+    
+}
