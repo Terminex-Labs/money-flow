5fd206c4c91f0e589468ae5abd4dded87a8f8a6b
texno stalin <texno.stalin@gmail.com>
2026-09-16 18:58:39 +0300

    fix: Исправлены репозитории с `DbConnection` на `IDbConnection`

--- dotnet/src/services/Catalog/MoneyFlow.Catalog.Infrastructure/Persistence/Repositories/Currencies/CurrencyReadOnlyRepository.cs
+++ dotnet/src/services/Catalog/MoneyFlow.Catalog.Infrastructure/Persistence/Repositories/Currencies/CurrencyReadOnlyRepository.cs
@@ -1,12 +1,12 @@
+using System.Data;
 using Shared.Dapper;
-using System.Data.Common;
 using Shared.Catalog.Contracts.Response;
 using MoneyFlow.Catalog.Infrastructure.Persistence.Constants;
 using MoneyFlow.Catalog.Application.Abstractions.Repositories.Currencies;
 
 namespace MoneyFlow.Catalog.Infrastructure.Persistence.Repositories.Currencies
 {
-    internal sealed class CurrencyReadOnlyRepository(DbConnection connection) : ReadOnlyRepository<CurrencyResponse>(connection, TableNames.Currency), ICurrencyReadOnlyRepository
+    internal sealed class CurrencyReadOnlyRepository(IDbConnection connection) : ReadOnlyRepository<CurrencyResponse>(connection, TableNames.Currency), ICurrencyReadOnlyRepository
     {
         
     }
--- dotnet/src/services/Catalog/MoneyFlow.Catalog.Infrastructure/Persistence/Repositories/TypeAccounts/TypeAccountReadOnlyRepository.cs
+++ dotnet/src/services/Catalog/MoneyFlow.Catalog.Infrastructure/Persistence/Repositories/TypeAccounts/TypeAccountReadOnlyRepository.cs
@@ -1,12 +1,12 @@
+using System.Data;
 using Shared.Dapper;
-using System.Data.Common;
 using Shared.Catalog.Contracts.Response;
 using MoneyFlow.Catalog.Infrastructure.Persistence.Constants;
 using MoneyFlow.Catalog.Application.Abstractions.Repositories.TypeAccounts;
 
 namespace MoneyFlow.Catalog.Infrastructure.Persistence.Repositories.TypeAccounts
 {
-    internal sealed class TypeAccountReadOnlyRepository(DbConnection connection) : ReadOnlyRepository<TypeAccountResponse>(connection, TableNames.TypeAccount), ITypeAccountReadOnlyRepository
+    internal sealed class TypeAccountReadOnlyRepository(IDbConnection connection) : ReadOnlyRepository<TypeAccountResponse>(connection, TableNames.TypeAccount), ITypeAccountReadOnlyRepository
     {
         
     }
