b47a68ca1a73b8b9e56016e916d3c43b31254669
texno stalin <texno.stalin@gmail.com>
2026-09-16 19:01:27 +0300

    feat: Создана миграция для сервиса `Catalog`

---
+++ dotnet/src/services/Catalog/MoneyFlow.Catalog.Infrastructure/Persistence/Migrations/20260916133824_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyFlow.Catalog.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: @"CREATE COLLATION case_insensitive (
+                    PROVIDER = 'icu',
+                    LOCALE = 'und-u-ks-level2',
+                    DETERMINISTIC = FALSE);",
+                suppressTransaction: true);
+
+            migrationBuilder.CreateTable(
+                name: "currencies",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    short_name = table.Column<string>(type: "text", nullable: false, collation: "case_insensitive"),
+                    unicode = table.Column<string>(type: "text", nullable: false, collation: "case_insensitive"),
+                    full_name = table.Column<string>(type: "text", nullable: false, collation: "case_insensitive")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_currencies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "type_accounts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false, collation: "case_insensitive")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_type_accounts", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "currencies");
+
+            migrationBuilder.DropTable(
+                name: "type_accounts");
+        }
+    }
+}
