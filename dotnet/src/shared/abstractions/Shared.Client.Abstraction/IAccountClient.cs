0e2b4d5859dbf8954fdc1f7f66de5f48a751d506
texno stalin <texno.stalin@gmail.com>
2026-09-18 21:33:05 +0300

    feat: Создан `HttpClient` для счетов

---
+++ dotnet/src/shared/abstractions/Shared.Client.Abstraction/IAccountClient.cs
@@ -0,0 +1,17 @@
+using Terminex.Common.Results;
+using Terminex.Common.Primitives;
+using Shared.Ledger.Contracts.Accounts.Request;
+using Shared.Ledger.Contracts.Accounts.Response;
+
+namespace Shared.Client.Abstraction
+{
+    public interface IAccountClient
+    {
+        Task<Result<CreatedAccountResponse>> CreateAsync(CreateAccountRequest request, CancellationToken ctn = default);
+        Task<Result<List<AccountResponse>>> GetAllAsync(CancellationToken ctn = default);
+        Task<Result<Nothing>> UpdateNameAsync(UpdateAccountNameRequest request, CancellationToken ctn = default);
+        Task<Result<Nothing>> FreezeAsync(Guid id, CancellationToken ctn = default);
+        Task<Result<Nothing>> UnfreezeAsync(Guid id, CancellationToken ctn = default);
+        Task<Result<Nothing>> DeleteAsync(Guid id, CancellationToken ctn = default);
+    }
+}
