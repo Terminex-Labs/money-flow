ea035ec171807f22f451357ba39f28c32eca9155
texno stalin <texno.stalin@gmail.com>
2026-09-18 21:29:48 +0300

    fix: Регистрация `Dapper` с `AddSingleton` на `AddTransient`

--- dotnet/src/services/Ledger/MoneyFlow.Ledger.Infrastructure/Extensions/DataBaseServiceCollectionExtension.cs
+++ dotnet/src/services/Ledger/MoneyFlow.Ledger.Infrastructure/Extensions/DataBaseServiceCollectionExtension.cs
@@ -22,7 +22,7 @@ namespace MoneyFlow.Ledger.Infrastructure.Extensions
         public static IServiceCollection UseDapper(this IServiceCollection services, IConfiguration configuration)
         {
             Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-            services.AddSingleton<IDbConnection>(serviceProvider => new NpgsqlConnection(configuration["DataBase:ConnectionStrings:Postgres"]));
+            services.AddTransient<IDbConnection>(serviceProvider => new NpgsqlConnection(configuration["DataBase:ConnectionStrings:Postgres"]));
 
             return services;
         }
