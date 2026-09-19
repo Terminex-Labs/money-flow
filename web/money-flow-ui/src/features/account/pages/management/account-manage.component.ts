1e04c56238009f10f377394882fac965fc535a86
texno stalin <texno.stalin@gmail.com>
2026-09-19 14:28:22 +0300

    feat: Реализована фича создания и отображения счетов

--- web/money-flow-ui/src/features/account/pages/management/account-manage.component.ts
+++ web/money-flow-ui/src/features/account/pages/management/account-manage.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
+import { Router } from "@angular/router";
+import { AccountService } from "../../services/account.service";
+import { AccountResponse } from "../../models/account.model";
 
 @Component({
     selector: 'app-account-manage',
@@ -8,5 +11,20 @@ import { Component } from "@angular/core";
 })
 
 export class AccountManageComponent {
+    private readonly accountService = inject(AccountService);
+    private router = inject(Router);
+
+    accountData = signal<AccountResponse[] | null>([]);
+
+    constructor() {
+        this.initializationAsync();
+    }
     
+    async initializationAsync() {
+        this.accountData.set(await this.accountService.getAllAccount());
+    }
+
+    navigate() {
+        this.router.navigate(['/account/form']);
+    }
 }
